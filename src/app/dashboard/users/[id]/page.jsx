342b1b06adc33fce68c593faec3383633913874e
JereUR <jeremias.jdv@gmail.com>
2024-03-14 21:38:10 -0300

    feat(Root): Edit user done

--- src/app/dashboard/users/[id]/page.jsx
+++ src/app/dashboard/users/[id]/page.jsx
@@ -1,41 +1,64 @@
+import Image from 'next/image'
+
 import { updateUser } from '@/app/services/actions'
 import { fetchUser } from '@/app/services'
-import Image from 'next/image'
 
 const SingleUserPage = async ({ params }) => {
   const { id } = params
-  const user = await fetchUser(id)
+  const { user } = await fetchUser(id)
+  const { username, email, password, img, phone, address, isAdmin, isActive } =
+    user
   return (
     <div className="flex gap-12 mt-5">
       <div className="w-1/4 bg-gray-800 p-5 rounded-lg font-bold text-gray-300 h-max">
         <div className="w-full h-80 relative rounded-lg overflow-hidden mb-5">
           <Image
-            src={user.img ? user.img : '/noavatar.png'}
-            alt={`${user.username} image`}
+            src={img ? img : '/noavatar.png'}
+            alt={`${username} image`}
             fill
           />
         </div>
-        {user.username}
+        {username}
       </div>
       <div className="w-3/4 bg-gray-800 p-5 rounded-lg">
         <form action={updateUser} className="user-form flex flex-col">
-          <input type="hidden" name="id" value={user.id} />
+          <input type="hidden" name="id" value={id} />
           <label>Username</label>
-          <input type="text" name="username" placeholder={user.username} />
+          <input
+            type="text"
+            name="username"
+            placeholder={username}
+            defaultValue={username}
+          />
           <label>Email</label>
-          <input type="email" name="email" placeholder={user.email} />
+          <input
+            type="email"
+            name="email"
+            placeholder={email}
+            defaultValue={email}
+          />
           <label>Password</label>
-          <input type="text" name="password" />
+          <input type="password" name="password" defaultValue={password} />
+          <input type="hidden" name="originalPassword" value={password} />
           <label>Phone</label>
-          <input type="text" name="phone" placeholder={user.phone} />
+          <input
+            type="text"
+            name="phone"
+            placeholder={phone}
+            defaultValue={phone}
+          />
           <label>Address</label>
-          <textarea name="address" placeholder={user.address}></textarea>
+          <textarea
+            name="address"
+            placeholder={address}
+            defaultValue={address}
+          ></textarea>
           <label>Is Admin?</label>
           <select
             name="isAdmin"
             id="isAdmin"
             className="cursor-pointer"
-            defaultValue={user.isAdmin}
+            defaultValue={isAdmin}
           >
             <option value={true}>Yes</option>
             <option value={false}>No</option>
@@ -45,7 +68,7 @@ const SingleUserPage = async ({ params }) => {
             name="isActive"
             id="isActive"
             className="cursor-pointer"
-            defaultValue={user.isActive}
+            defaultValue={isActive}
           >
             <option value={true}>Yes</option>
             <option value={false}>No</option>
