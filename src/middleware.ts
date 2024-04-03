9d2c2997f4e9b9aff074e2afdbf46b6cd25568c8
JereUR <jeremias.jdv@gmail.com>
2024-04-02 21:53:36 -0300

    feat(Root): Authentication with clerk

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,7 @@
-// middleware/authMiddleware.js
-import { NextRequest, NextResponse } from 'next/server'
-import { createMiddlewareSupabaseClient } from './app/services/supabase' // Importa el cliente de Supabase
-import { NextApiResponse, NextApiRequest } from 'next'
+import { authMiddleware } from '@clerk/nextjs'
 
-const protectedRoutes = ['/dashboard']
+export default authMiddleware({})
 
-export default async function Middleware(req: NextRequest, res: NextResponse) {
-  const supabase = createMiddlewareSupabaseClient({ req })
-  const { data } = await supabase.auth.getSession()
-  const { session } = data
-  const isDashboardRoute = req.nextUrl.pathname.match(/^\/dashboard/)
-  /* 
-  if (
-    !session &&
-    (protectedRoutes.includes(req.nextUrl.pathname) || isDashboardRoute)
-  ) {
-    const absoluteURL = new URL('/login', req.nextUrl.origin)
-    return NextResponse.redirect(absoluteURL.toString())
-  } */
+export const config = {
+  matcher: ['/((?!.+.[w]+$|_next).*)', '/', '/(api|trpc)(.*)']
 }
